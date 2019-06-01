90b485c36f5f779c090f86ede08c049fd81de038
atfost3r <alexnader.tfoster@gmail.com>
2019-06-01 16:04:31 -0500

    Collatz function completed w/ stack overflow help

--- Chapter 3 Functions/collatz.py
+++ Chapter 3 Functions/collatz.py
@@ -1,25 +1,14 @@
-#This is to define the 
-
 def collatz(number):
-    numCheck = number % 2 
-    if numCheck == 1:
-        number = (3 * number) + 1
-        return int(number)
-    else:
-        number = (number // 2)
-        return int(number)
+    
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
 
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
 
-
-
-#This is the user input section and loop until we get our answer
-
-print('Time to kick off the Collatz problem!')
-print('Please enter a number:')
-number = int(input())
-
-collatz(number)
-
-while number != 1:
-    collatz(number)
-
+n = input("Give me a number: ")
+while n != 1:
+    n = collatz(int(n)
