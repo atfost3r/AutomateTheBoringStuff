d47929c2fe759755a26704900c8ef64be65990a4
Alex Foster <alexander.tfoster@gmail.com>
2020-05-19 22:10:34 -0500

    initial commit

---
+++ AutomatingTasks/Chapter 7/phoneAndEmail.py
@@ -0,0 +1,26 @@
+!# python3
+#phoneAndEmail.py - Finds the phone numbers and email addresses on the clipboard
+
+import pyperclip, re
+
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(d{3}\))?               # area code
+    (\s|-|\.)?                      # seperator
+    (\d{3})                         # first 3 digits
+    (\s|-|\.)                       # second 3 digits
+    (\d{4})                         # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+    )''', re.VERBOSE)
+
+# Create Email REGEX
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+       # username
+    @                       # at symbol
+    [a-zA-Z0-9.-]+          # domain name
+    (\.[a-zA-Z](2,4))       # dot something
+    )''', re.VERBOSE)
+
+# TODO: Find matches in clipboard text
+
+# TODO: Copy results to clipboard
