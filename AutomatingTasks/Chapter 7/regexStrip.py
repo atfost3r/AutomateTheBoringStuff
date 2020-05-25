f2f1faac3005b849676bf5d9ca6e91a100449f79
atfost3r <alexander.tfoster@gmail.com>
2020-05-25 14:31:09 -0500

    save

--- AutomatingTasks/Chapter 7/regexStrip.py
+++
@@ -1,9 +0,0 @@
-#! python3
-
-#This function takes a string and does the same thing as the strip() strong method.
-
-#TODO: Check inputs. If no other argument, remove whitespace before and after string
-
-
-
-#TODO: Remove the characters from the input string
