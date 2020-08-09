459314e9a2034041c46bf7d16812a66d7ac3201a
Alex Foster <alexander.tfoster@gmail.com>
2020-08-08 21:57:03 -0500

    Python Webscraping added

--- AutomatingTasks/Chapter 10/factorialLog.py
+++ AutomatingTasks/Chapter 10/factorialLog.py
@@ -5,12 +5,12 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 logging.debug('Start of program')
 
 def factorial(n):
-    logging.debug('Start of factorial(%s%%)' % (n))
+    logging.debug('Start of factorial(%s%%)'  % (n))
     total = 1
     for i in range(1, n + 1):
         total *= i
         logging.debug('i is ' + str(i) + ', total is ' + str(total))
-    logging.debug('End of factorial(%s%%)' % (n))
+    logging.debug('End of factorial(%s%%)'  % (n))
     return total
 
 print(factorial(5))
