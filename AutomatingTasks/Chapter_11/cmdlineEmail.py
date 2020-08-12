0b35542ccb44b22c37e9f5a41d118f7ddcfeb80d
atfost3r <alexander.tfoster@gmail.com>
2020-08-11 20:37:08 -0500

    web scraping stuff

---
+++ AutomatingTasks/Chapter_11/cmdlineEmail.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+#cmdlineEmail.py - takes a command line input of an email address and string of text then opens a web browser, logs in, and sends email to email address. 
+
+#TODO: Get input from command line
+
+#TODO: Open webbeowser
+
+#TODO: naigate to email account homepage
+
+#TODO: Log in to my account
+
