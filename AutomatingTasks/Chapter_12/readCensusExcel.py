1a6f9fc0fc0d42e757270636fc14e8c71ea38d60
Alex Foster <alexander.tfoster@gmail.com>
2020-09-08 21:05:04 -0500

    Excel stuff

---
+++ AutomatingTasks/Chapter_12/readCensusExcel.py
@@ -0,0 +1,18 @@
+#! python3
+
+# readCencusExcel.py - Tabulates population and the number of cencus tracts for each country
+
+import openpyxl, pprint
+print('Opening workbook...')
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb.get_sheet_by_name('Population by Census Tract')
+countyData = {}
+
+#TODO: Fill in countryData with each contry's population and tracts.
+print('Reading rows...')
+for row in range(2, sheet.get_highest_row() + 1):
+    state   = sheet['B' + str(row)].value
+    county  = sheet['C' + str(row)].value
+    pop     = sheet['D' + str(row)].value
+
+# TODO: Open a new text file and write the contents of countryData to it. 
