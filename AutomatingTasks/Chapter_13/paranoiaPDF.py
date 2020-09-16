275ff7b478cd8c8b05aa65f20415113c671e732e
Alex Foster <alexander.tfoster@gmail.com>
2020-09-15 21:55:56 -0500

    Project start

---
+++ AutomatingTasks/Chapter_13/paranoiaPDF.py
@@ -0,0 +1,21 @@
+#! python3 
+#paranoiaPDF.py - Walks through the every PDF in a folder and all the subfolders and 
+# creates an encrpyted version of the PDF using a password provided by the user via the commandline
+
+import PyPDF2, os 
+
+password = sys.argv[:]
+
+#TODO: Set up os.walk() 
+
+#TODO: Get password from command line
+
+#TODO: find all PDF files
+
+#TODO: encrypt PDF with input password
+
+#TODO: append _encrypted.pdf to the end of encrypted files
+
+#TODO: Test each file that succussfully encrypted
+
+#Save and close file
