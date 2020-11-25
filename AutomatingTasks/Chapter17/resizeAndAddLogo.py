2cf8e010f5985eaf7e28aefa599d97f0a5064c2e
Alex Foster <alexander.tfoster@gmail.com>
2020-11-24 21:26:44 -0600

    Update as of 11/24

--- AutomatingTasks/Chapter17/resizeAndAddLogo.py
+++ AutomatingTasks/Chapter17/resizeAndAddLogo.py
@@ -19,4 +19,16 @@ for filename in os.listdir('.'):
 
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
         if width > height:
-            
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
+
+        print('Resizing %s...' % (filename))
+        im = im.resize((width, height))
+
+        print('Adding logo to %s...' % (filename))
+        im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+        im.save(os.path.join('withLogo', filename))
+        
---
+++ AutomatingTasks/Chapter18/formFiller.py
@@ -0,0 +1,8 @@
+#! python3
+
+#formFiller.py - Automatically fill out the form
+
+import pyautogui, time
+
+#Set the mouse coordinates
+nameField = ()
